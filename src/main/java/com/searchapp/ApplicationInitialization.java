14ae6eff93e29f1654e4ab28a818b527347907db
indiasaurabh@hotmail.com <indiasaurabh@hotmail.com>
2017-07-07 01:31:36 +0530

    redis session

--- src/main/java/com/searchapp/ApplicationInitialization.java
+++ src/main/java/com/searchapp/ApplicationInitialization.java
@@ -1,13 +1,9 @@
 package com.searchapp;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
-//@ComponentScan({"com.searchapp.controller", "com.searchapp.dao"})
 public class ApplicationInitialization {
 
 	public static void main(String[] args) {
